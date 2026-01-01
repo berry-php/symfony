bb1ceeef642fab8b1a5fd2cee0f0bd2cf4490e66
Christopher Kaster <me@atomicptr.de>
2026-01-01 16:37:54 +0100

    allow custom renderers with BerryControllerTrait, add support for returning StreamedResponse-s

--- src/Controller/BerryControllerTrait.php
+++ src/Controller/BerryControllerTrait.php
@@ -3,8 +3,11 @@
 namespace Berry\Symfony\Controller;
 
 use Berry\Rendering\ArrayBufferRenderer;
+use Berry\Rendering\DirectOutputRenderer;
+use Berry\Rendering\StringRenderer;
 use Berry\Element;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 trait BerryControllerTrait
 {
@@ -13,11 +16,35 @@ trait BerryControllerTrait
      *
      * @param array<string, string> $headers
      */
-    protected function renderBerryView(Element $renderable, int $statusCode = 200, array $headers = []): Response
+    protected function renderBerryView(Element $element, int $statusCode = 200, array $headers = [], ?StringRenderer $renderer = null): Response
     {
-        $renderer = new ArrayBufferRenderer();
-        $renderable->render($renderer);
+        $renderer ??= new ArrayBufferRenderer();
+        $element->render($renderer);
 
         return new Response($renderer->renderToString(), $statusCode, $headers);
+    }
+
+    /**
+     * Renders a streamed view to the client
+     *
+     * @param array<string, string> $headers
+     */
+    protected function renderStreamedBerryView(Element $element, int $statusCode = 200, array $headers = []): Response
+    {
+        return new StreamedResponse(
+            function () use ($element) {
+                $renderer = new DirectOutputRenderer();
+                $element->render($renderer);
+
+                flush();
+            },
+            $statusCode,
+            array_merge(
+                [
+                    'X-Accel-Buffering' => 'no'
+                ],
+                $headers,
+            )
+        );
     }
 }
