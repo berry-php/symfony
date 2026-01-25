04a142c7c3fd924ecf59ba80798c907810e3d7ac
Christopher Kaster <me@atomicptr.de>
2026-01-25 17:28:11 +0100

    update to support berry 0.14

--- src/Controller/BerryControllerTrait.php
+++ src/Controller/BerryControllerTrait.php
@@ -2,60 +2,22 @@
 
 namespace Berry\Symfony\Controller;
 
-use Berry\Rendering\DirectOutputRenderer;
-use Berry\Rendering\StringConcatRenderer;
-use Berry\Rendering\StringRenderer;
 use Berry\Element;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 trait BerryControllerTrait
 {
-    /**
-     * Renders a view as a string
-     */
-    protected function renderBerryViewAsString(Element $element, ?StringRenderer $renderer = null): string
-    {
-        $renderer ??= new StringConcatRenderer();
-        $element->render($renderer);
-        return $renderer->renderToString();
-    }
-
     /**
      * Renders a view.
      *
      * @param array<string, string> $headers
      */
-    protected function renderBerryView(Element $element, int $statusCode = 200, array $headers = [], ?StringRenderer $renderer = null): Response
+    protected function renderBerryView(Element $element, int $statusCode = 200, array $headers = []): Response
     {
         return new Response(
-            $this->renderBerryViewAsString($element, $renderer),
+            $element->toString(),
             $statusCode,
             $headers,
-        );
-    }
-
-    /**
-     * Renders a streamed view to the client
-     *
-     * @param array<string, string> $headers
-     */
-    protected function renderStreamedBerryView(Element $element, int $statusCode = 200, array $headers = []): Response
-    {
-        return new StreamedResponse(
-            function () use ($element) {
-                $renderer = new DirectOutputRenderer();
-                $element->render($renderer);
-
-                flush();
-            },
-            $statusCode,
-            array_merge(
-                [
-                    'X-Accel-Buffering' => 'no'
-                ],
-                $headers,
-            )
         );
     }
 }
--- tests/Unit/BerryControllerTraitTest.php
+++ tests/Unit/BerryControllerTraitTest.php
@@ -2,7 +2,6 @@
 
 use Berry\Symfony\Controller\BerryControllerTrait;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 use function Berry\Html\button;
 
@@ -20,22 +19,4 @@ test('renderBerryView returns a Symfony Response with rendered content', functio
     expect($response->getStatusCode())->toBe(201);
     expect($response->headers->get('X-Custom'))->toBe('Yes');
     expect($response->getContent())->toBe('<button class="btn btn-primary">Hello, World!</button>');
-});
-
-test('renderStreamedBerryView returns a StreamedResponse', function () use ($testController) {
-    $refl = new ReflectionMethod($testController, 'renderStreamedBerryView');
-
-    /** @var Response */
-    $response = $refl->invoke($testController, button()->class('btn btn-primary')->text('Hello, World!'), 201, ['X-Custom' => 'Yes']);
-
-    expect($response)
-        ->toBeInstanceOf(StreamedResponse::class)
-        ->and($response->headers->get('X-Accel-Buffering'))
-        ->toBe('no');
-
-    ob_start();
-    $response->sendContent();
-    $output = ob_get_clean();
-
-    expect($output)->toBe('<button class="btn btn-primary">Hello, World!</button>');
 });
