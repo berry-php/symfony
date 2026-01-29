72f05ede0e23875fb92b1bb6409ae36d6d0f0b59
Christopher Kaster <me@atomicptr.de>
2026-01-29 21:13:03 +0100

    add test for translation

--- tests/Unit/BerryExtensionsTest.php
+++ tests/Unit/BerryExtensionsTest.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 use function Berry\Html\div;
 use function Berry\Html\h1;
 
@@ -16,6 +18,29 @@ test('Extension: toResponse', function () {
     expect($res->getContent())->toBe('<h1>Page not found!</h1>');
 });
 
-test('Test dumper', function () {
+test('Extension: trans', function () {
+    $translator = new class implements TranslatorInterface {
+        /**
+         * @param array<string, mixed> $parameters
+         */
+        public function trans(string $id, array $parameters = [], ?string $domain = null, ?string $locale = null): string
+        {
+            if ($id === 'some.key') {
+                return 'Hello, World!';
+            }
+
+            return '???';
+        }
+
+        public function getLocale(): string
+        {
+            return 'en';
+        }
+    };
+
+    expect(div()->trans('some.key', translator: $translator)->toString())->toBe('<div>Hello, World!</div>');
+});
+
+test('Extension: dump', function () {
     expect(div()->text('Hello, World!')->dump('swaggy')->toString())->toContain('sf-dump');
 });
